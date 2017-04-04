4c75496763287877c275be50382a8f97fd6cc17b
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-04-04 00:11:04 -0600

    Added other operations

--- src/main/java/com/example/MathCalculateController.java
+++ src/main/java/com/example/MathCalculateController.java
@@ -12,10 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathCalculateController {
 
-    public int result;
+    public String result;
     @GetMapping("/math/calculate")
-    public String getOperationAdd(@RequestParam String operation , int x, int y) {
-        return String.format("%s + %s = %s",x, y, x+y);
+    public String getCalculate(@RequestParam int x, int y, String operation) {
+
+        if (operation == null || operation.equals("add")) {
+            result = String.format("%s + %s = %s",x, y, x + y);
+        } else if (operation.equals("multiply")) {
+            result = String.format("%s * %s = %s",x, y, x * y);
+        } else if (operation.equals("substract")) {
+            result = String.format("%s - %s = %s",x, y, x - y);
+        } else if (operation.equals("divide")) {
+            result = String.format("%s / %s = %s",x, y, x / y);
+        }
+    return result;
     }
 
 }
